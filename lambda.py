806eed4083eddbf7ab578e3a2d7064404db0895b
EgeDeniz <163152609+denizege@users.noreply.github.com>
2024-03-19 19:08:18 +0200

    Update 6 lambda.py

--- lambda.py
+++ lambda.py
@@ -36,3 +36,13 @@ words = ["apple", "banana", "cherry", "date", "fig"]
 letter = input()
 count = len(list(filter(lambda word: letter in word, words)))
 print(count)
+
+
+# Напиши лямбда-выражение, которое принимает строку и возвращает ее первую букву (в верхнем регистре).
+# Затем используй лямбда-выражение для преобразования списка слов в список их первых букв.
+# Например, из списка ["apple", "banana", "cherry"] должно получиться ['A', 'B', 'C'].
+
+words = ["apple", "banana", "cherry", "date", "fig"]
+get_first_letter = lambda x: x[0].upper()
+first_letters = list(map(get_first_letter, words))
+print(first_letters)
